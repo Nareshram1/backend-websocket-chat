12ca481ae2cfc177484cc5d9dfc2110be873881e
Nareshram1 <nireshram805s1@gmail.com>
2024-01-26 19:02:39 +0530

    first commit

---
+++ server/server.ts
@@ -0,0 +1,29 @@
+declare var require:any
+const express = require('express')
+const http = require('http')
+
+
+const app=express()
+const server=http.createServer(app)
+
+import {Server} from 'socket.io'
+
+const io= new Server(server,{
+    cors:{
+        origin:'*',
+        methods:['POST','GET']
+    },
+})
+const clients:string[]=[]
+io.on('connection',async(socket)=>{
+    console.log(socket.id)
+    clients.push(socket.id)
+    socket.on('send-msg',(msg:string)=>{
+        console.log(msg)
+        socket.broadcast.emit('rec-msg',msg)
+    })
+})
+// io.emit("hello")
+server.listen(5000,()=>{
+    console.log("nodemon at port 5000");
+})
