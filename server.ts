e41498de04ea59f9c0e5b8ab623fb24737099c53
Nareshram1 <nireshram805s1@gmail.com>
2024-07-19 13:45:18 +0530

    improve TS and fix CORS fix

--- server.ts
+++ server.ts
@@ -1,37 +1,39 @@
-declare var require:any
-const express = require('express')
-const http = require('http')
-const cors =require('cors')
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { Server } from 'socket.io';
 
-const app=express()
-const server=http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 
-import {Server} from 'socket.io'
+app.use(cors());
 
-const io= new Server(server,{
-    cors:{
-        origin: "*",
+const io = new Server(server, {
+    cors: {
+        origin: "https://simbunewyear.vercel.app",
         methods: ['POST', 'GET'],
     },
-})
-// const clients:string[]=[]
-console.log("server live")
-io.on('connection',async(socket)=>{
-    console.log(socket.id)
-    // inital call
+});
+
+console.log("Server live");
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+    
+    // Initial call
     socket.emit('connected', io.engine.clientsCount);
-    // when a client connects ,send the number of connected users to all clients
-    socket.on('send-msg',(msg:string)=>{
-        socket.broadcast.emit('rec-msg',msg,io.engine.clientsCount)
-    })
-    //  disconnect 
-    socket.on("disconnect",()=>{
+
+    // When a client connects, send the number of connected users to all clients
+    socket.on('send-msg', (msg: string) => {
+        socket.broadcast.emit('rec-msg', msg, io.engine.clientsCount);
+    });
+
+    // Disconnect
+    socket.on('disconnect', () => {
         console.log(`User disconnected: ${socket.id}`);
-    })
-})
+    });
+});
 
-server.listen(5000,()=>{
-    console.log("nodemon at port 5000");
-
-})
-
+server.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
